9dea63a03250deccef7fedcface4e43dc480f8e0
Anupam <mirakib25@gmail.com>
2022-10-29 17:58:42 +0600

    g

--- src/components/student/registration/Registration.js
+++ src/components/student/registration/Registration.js
@@ -17,6 +17,7 @@ export default function Registration() {
             })
             .catch((error) => {
                 console.error(error)
+                alert(error)
                 console.log("not found")
             })
     }, [])
